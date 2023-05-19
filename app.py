0dc2f57529a1a34c297bb440df7dc28ca3e1e32b
Alvrzco <a.corujo@udc.es>
2023-05-20 00:29:17 +0200

    hola

--- app.py
+++ app.py
@@ -375,7 +375,7 @@ def hook():
                                 tup = (conversation_id,mobile)
                                 mobile_tup = (mobile,)
                                 #update y enviar mensaje nuevo
-                                messenger.send_message(f"Soy EventBot 🤖, tu asistente personal durante todo el *Wake Up & Dream*. Soy un poco torpe y a las 24h me reinicio para descansar y olvido toda nuestra conversación 😇. Toda la información que necesitas está disponible a través del *MENÚ PRINICPAL* que aparece a continuación.",mobile)
+                                #messenger.send_message(f"Soy EventBot 🤖, tu asistente personal durante todo el *Wake Up & Dream*. Soy un poco torpe y a las 24h me reinicio para descansar y olvido toda nuestra conversación 😇. Toda la información que necesitas está disponible a través del *MENÚ PRINICPAL* que aparece a continuación.",mobile)
                                 cursor = connection.cursor()
                                 cursor.execute('''UPDATE wakeup_bot SET last_conver = %s WHERE phone = %s''',tup)
                                 cursor.execute('''UPDATE wakeup_bot SET check24h = 1 WHERE phone = %s''',mobile_tup)
