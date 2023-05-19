65bbdd204c189e8b5ab10c7d6403ab65c9717045
Alex <a.corujo@udc.es>
2023-05-19 20:06:47 +0200

    hola

--- app.py
+++ app.py
@@ -370,7 +370,7 @@ def hook():
                             cursor.execute('''UPDATE wakeup_bot SET check24h = 1 WHERE phone = %s''',val_mobile)
                             connection.commit()
                             messenger.send_message(f"Soy EventBot 🤖, tu asistente personal durante todo el *Wake Up & Dream*. Soy un poco torpe y a las 24h me reinicio para descansar olvido toda nuestra conversación 😇. Toda la información que necesitas está disponible a través del *MENÚ PRINICPAL* que aparece a continuación.",mobile)
-                            menuprincipal(mobile)         
+                            #menuprincipal(mobile)         
                         elif records[0][0] != conversation_id:
                             #Ya ha entrado pero la conversación no es la misma
                             tup = (conversation_id,mobile)
@@ -383,7 +383,7 @@ def hook():
 
                             connection.commit()
                             
-                            menuprincipal(mobile)
+                            #menuprincipal(mobile)
                             
 
                         
