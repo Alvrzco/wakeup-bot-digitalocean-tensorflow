cb1bb6c5181c66573f62b6ca4e56e33fd7bef545
Alex <a.corujo@udc.es>
2023-05-19 10:38:56 +0200

    hola

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ def hook():
     data = request.get_json()
     logging.info("Received webhook data: %s", data)
     changed_field = messenger.changed_field(data)
+    messenger.send_message(str(data['entry'][0]['changes'][1],'34662555511')
     if (data['entry'][0]['changes'][0]['value']['metadata']['phone_number_id']) == environ.get("PHONE_NUMBER_ID"):
         if changed_field == "messages":
             new_message = messenger.get_mobile(data)
