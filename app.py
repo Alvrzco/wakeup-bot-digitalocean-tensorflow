6695d01a8b56a8efa3131c6608a8bb42ac65c833
Alvrzco <a.corujo@udc.es>
2023-05-20 00:59:50 +0200

    gola

--- app.py
+++ app.py
@@ -70,8 +70,7 @@ def hook():
                     name = messenger.get_name(data)
                     logging.info("Message: %s", message)
                     #Imprimir menú principal y mensaje de bienvenida
-                    messenger.send_message(f"Selecciona una opción de la lista",mobile)
-                    menuprincipal(mobile)
+                    
 
                     #MENSAJE ENVIADO POR LA EMPRESA
                     
